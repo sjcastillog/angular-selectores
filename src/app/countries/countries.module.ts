ffbca08abeaad9c1d60d3fec28ad7b5fd77a119b
Joao Castillo <joaocastillo91@gmail.com>
2023-06-27 15:05:04 -0500

    first commit 27-06-2023

--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SelectorPageComponent } from './pages/selector-page/selector-page.component';
 import { CountriesRoutingModule } from './countries-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
 
+import { SelectorPageComponent } from './pages/selector-page/selector-page.component';
 
 
 @NgModule({
--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,20 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CountriesService } from '../../services/countries.service';
+import { Region, SmallCountry } from '../../interfaces/country.interfaces';
+import { filter, switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-selector-page',
   templateUrl: './selector-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class SelectorPageComponent {
-  public myForm:FormGroup = this.fb.group({
-    region:['', Validators.required],
-    country:['', Validators.required],
-    borders:['', Validators.required],
+export class SelectorPageComponent implements OnInit {
+  
+  public myForm: FormGroup = this.fb.group({
+    region: ['', Validators.required],
+    country: ['', Validators.required],
+    border: ['', Validators.required],
   });
 
+  public countriesByRegion:SmallCountry[] = [];
+  public borders:SmallCountry[]=[];
+
   constructor(
-    private fb:FormBuilder
-  ){}
+    private fb: FormBuilder,
+    private countriesService: CountriesService
+  ) {}
+
+  ngOnInit(): void {
+   this.onRegionChanged();
+   this.onCountryChanged();
+  }
+
+  get regions(): Region[] {
+    return this.countriesService.regions;
+  }
+
+
+  onRegionChanged():void{
+    this.myForm.get('region')!.valueChanges
+    .pipe(
+      tap( ()=> this.myForm.get('country')!.setValue("")),
+      tap( ()=> this.borders = []),
+      switchMap((region)=>this.countriesService.getCountriesByRegion(region))
+    )
+    .subscribe(countries => {
+      this.countriesByRegion = countries;
+    });
+  }
+
+  onCountryChanged():void{
+    this.myForm.get('country')!.valueChanges
+    .pipe(
+      tap( ()=> this.myForm.get('border')!.setValue("")),
+      filter( (value:string)=> value.length > 0),
+      switchMap((alphaCode)=>this.countriesService.getCountryByAlphaCode(alphaCode)),
+      switchMap((country)=>this.countriesService.getCountryBordersByCodes(country.borders)),
+    
+      )
+    .subscribe(countries => {
+      this.borders = countries;
+    });
+  }
+
 }
